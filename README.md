b50a60d7a4b2d2fb3a669dd061a0c7defb35791c
Lorenzo Bocchi <lorenzobocchi99@yahoo.com>
2023-03-15 12:00:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# text-summarizer
+
+TF-IDF text summarizer based on the code at https://github.com/holydrinker/text-summarizer/ and the paper [Centroid-based Text Summarization through Compositionality of Word Embeddings](www.aclweb.org/anthology/W/W17/W17-1003.pdf) by Gaetano Rossiello, Pierpaolo Basile and Giovanni Semeraro.
+
+The code was adapted to use a compressed fastText model instead of the original Word2Vec model to be used in an environment with limited resources.
+
+## How to use
+Clone the repository, and install the requirements:
+```bash
+pip install -r requirements.txt
+```
+
+Then, import the Summarizer:
+```python
+from summarizer import Summarizer
+```
+
+Then, create a Summarizer object:
+```python
+model = Summarizer()
+``` 
+
+Finally, summarize a text:
+```python
+model.summarize(text)
+```
+
+This will return a list of sentences, and a list of the corresponding scores. From this, you can order the scores and sentences, and return the top n sentences.
+
+## Where to get the model
+To get the compressed models, you can check https://github.com/avidale/compress-fasttext/releases/tag/gensim-4-draft and https://zenodo.org/record/4905385.
---
+++ summarizer.py
@@ -0,0 +1,248 @@
+# coding=utf-8
+import numpy as np
+from compress_fasttext.models import CompressedFastTextKeyedVectors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine
+from nltk.corpus import stopwords
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.tokenize import sent_tokenize
+from re import sub
+from os import path
+from copy import deepcopy
+
+
+class LookupTable:
+    def __init__(self, model_path):
+        self.model = CompressedFastTextKeyedVectors.load(path.abspath(model_path))
+
+    def vec_word(self, word):
+        """
+        Return the vector of a word if it is in the vocabulary, otherwise return a vector of zeros
+
+        :param word: word to get the vector
+        :return: vector of the word
+        """
+        try:
+            return self.model[word]
+        except KeyError:
+            return np.zeros(1)
+
+    def vec_sentence(self, sentence):
+        """
+        Return the vector of a sentence if it is in the vocabulary, otherwise return a vector of zeros
+
+        :param sentence: sentence to get the vector
+        :return: vector of the sentence
+        """
+        try:
+            return self.model.get_sentence_vector(sentence)
+        except KeyError:
+            return np.zeros(300)
+
+    def unseen(self, word):
+        """
+        Check if a word is in the vocabulary
+
+        :param word: word to check
+        :return: True if the word is not in the vocabulary, False otherwise
+        """
+        try:
+            self.model[word]
+            return False
+        except KeyError:
+            return True
+
+
+class Summarizer:
+    def __init__(
+        self,
+        model_path=None,
+        tfidf_threshold=0.3,
+        redundancy_threshold=0.9,
+        language="italian",
+    ):
+        """
+        :param model_path: path to the compressed fasttext model
+        :param tfidf_threshold: threshold to filter relevant terms
+        :param redundancy_threshold: threshold to filter redundant sentences
+        :param language: language of the text to summarize
+        """
+        self.lookup_table = LookupTable(model_path)
+        self.tfidf_threshold = tfidf_threshold
+        self.sentence_retriever = []
+        self.redundancy_threshold = redundancy_threshold
+        self.language = language
+
+    def _preprocessing(self, text):
+        """
+        Preprocess the text to summarize
+
+        :param text: text to summarize
+        :return: preprocessed text
+        """
+        # Get splitted sentences
+        sentences = self.get_data(text, self.language)
+
+        # Store the sentence before process them. We need them to build final summary
+        self.sentence_retriever = deepcopy(sentences)
+
+        # Remove punctuation and stopwords
+        sentences = self.remove_punctuation_nltk(sentences)
+        sentences = self.remove_stopwords(sentences)
+
+        return sentences
+
+    def _gen_centroid(self, sentences):
+        """
+        Generate the centroid of the document
+
+        :param sentences: sentences of the document
+        :return: centroid of the document
+        """
+        tf = TfidfVectorizer()
+        tfidf = tf.fit_transform(sentences).toarray().sum(0)
+        tfidf = np.divide(tfidf, tfidf.max())
+        words = tf.get_feature_names()
+
+        relevant_terms = [
+            words[i]
+            for i in range(len(tfidf))
+            if tfidf[i] >= self.tfidf_threshold
+            and not self.lookup_table.unseen(words[i])
+        ]
+
+        res = [self.lookup_table.vec_word(term) for term in relevant_terms]
+        return sum(res) / len(res)
+
+    def _sentence_vectorizer(self, sentences):
+        """
+        Vectorize the sentences of the document
+
+        :param sentences: sentences of the document
+        :return: dictionary of sentences vectorized
+        """
+        dic = {}
+        for i in range(len(sentences)):
+            sum_vec = np.zeros(self.lookup_table.model.vector_size)
+            sentence = [
+                word
+                for word in sentences[i].split(" ")
+                if not self.lookup_table.unseen(word)
+            ]
+
+            if sentence:
+                for word in sentence:
+                    word_vec = self.lookup_table.vec_word(word)
+                    sum_vec = np.add(sum_vec, word_vec)
+                dic[i] = sum_vec / len(sentence)
+        return dic
+
+    def _sentence_selection(self, centroid, sentences_dict):
+        """
+        Select the sentences of the summary
+        
+        :param centroid: centroid of the document
+        :param sentences_dict: dictionary of sentences vectorized
+        :return: ids+importance of the sentences of the document, sentences of the document
+        """
+        record = []
+        for sentence_id in sentences_dict:
+            vector = sentences_dict[sentence_id]
+            similarity = 1 - cosine(centroid, vector)
+            record.append((sentence_id, vector, similarity))
+
+        full_ids_importance = [(x[0], x[2]) for x in record]
+
+        full_phrases_list = list(
+            map(
+                lambda sent_id: self.sentence_retriever[sent_id],
+                map(lambda t: t[0], full_ids_importance),
+            )
+        )
+        return full_ids_importance, full_phrases_list
+
+    def summarize(self, text):
+        """
+        Summarize the text
+        
+        :param text: text to summarize
+        :return: ids+importance of the sentences of the document, sentences of the document
+        """
+        self._check_params(self.redundancy_threshold, self.tfidf_threshold)
+
+        # Sentences generation (with preprocessing) + centroid generation
+        sentences = self._preprocessing(text)
+
+        centroid = self._gen_centroid(sentences)
+
+        # Sentence vectorization + sentence selection
+        sentences_dict = self._sentence_vectorizer(sentences)
+        ids_importance, phrases = self._sentence_selection(centroid, sentences_dict)
+
+        return ids_importance, phrases
+
+    def remove_punctuation_nltk(self, data):
+        """
+        Remove punctuation from the sentences
+        
+        :param data: sentences to process
+        :return: sentences without punctuation
+        """
+        to_return = []
+        for sentence in data:
+            temp = ""
+            tokenized = TreebankWordTokenizer().tokenize(sentence.lower())
+            for word in tokenized:
+                temp += word
+                temp += " "
+            to_return.append(temp[:-1])
+        return to_return
+
+    def remove_stopwords(self, data):
+        """
+        Remove stopwords from the sentences
+        
+        :param data: sentences to process
+        :return: sentences without stopwords
+        """
+        to_return = []
+        stop = set(stopwords.words(self.language))
+        for sentence in data:
+            stopped = ""
+            sentence = sentence.lower().split(" ")
+            temp = [i for i in sentence if i not in stop]
+            for word in temp:
+                stopped += word
+                stopped += " "
+            to_return.append(stopped)
+        return to_return
+
+    def get_data(self, text, language):
+        """
+        Split the text into sentences
+        
+        :param text: text to split
+        :param language: language of the text
+        :return: sentences of the text
+        """
+        sentences = sent_tokenize(text, language)
+        fixed_sentences = [self._fix_sentence(s) for s in sentences]
+        return fixed_sentences
+
+    @staticmethod
+    def _fix_sentence(sentence):
+        sentence = sentence.replace("\n", " ")
+        sentence = sub(" +", " ", sentence).strip()
+        return sentence
+
+    @staticmethod
+    def _check_params(redundancy, tfidf):
+        try:
+            assert 0 <= redundancy <= 1
+        except AssertionError:
+            raise ("ERROR: the redundancy threshold is not valid")
+
+        try:
+            assert 0 <= tfidf <= 1
+        except AssertionError:
+            raise ("ERROR: the tfidf threshold is not valid")
